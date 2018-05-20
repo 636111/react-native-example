36d7c256edcd47c2983a6d0ee3b9df35070a5f82
Carmen Domínguez Campillo <>
2018-05-20 22:49:30 +0200

    Ass HelloWorld component

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HelloWorld from './components/HelloWorld';
 
-export default class HelloWorldApp extends Component {
+export default class App extends React.Component {
   render() {
     return (
-      <Text>Hello world</Text>
+      <View style={styles.container}>
+        <HelloWorld />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
