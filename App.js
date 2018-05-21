b009757106ec4e3b05460685b53b5dff0b4800f8
Carmen Domínguez Campillo <>
2018-05-21 21:02:29 +0200

    Add FixedDimensionsBasics component

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import LotsOfStyles from './components/LotsOfStyles';
+import FixedDimensionsBasics from './components/FixedDimensionsBasics';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <LotsOfStyles />
+        <FixedDimensionsBasics />
       </View>
     );
   }
