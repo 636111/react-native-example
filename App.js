2028fe23f355ac643b333ee8498a4079d0a33ceb
Carmen Domínguez Campillo <>
2018-05-22 23:07:23 +0200

    Add FlatListBasics component

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import IScrolledDownAndWhatHappenedNextShockedMe from './components/IScrolledDownAndWhatHappenedNextShockedMe';
+import FlatListBasics from './components/FlatListBasics';
 
 export default class App extends React.Component {
   render() {
     return (
-      <IScrolledDownAndWhatHappenedNextShockedMe />
+      <FlatListBasics />
     );
   }
 }
