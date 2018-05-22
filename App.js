a32244b31bf3d8bc769b5ad47b22b567f8914f57
Carmen Domínguez Campillo <>
2018-05-22 22:18:02 +0200

    Add Touchables component

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import ButtonBasics from './components/ButtonBasics';
+import Touchables from './components/Touchables';
 
 export default class App extends React.Component {
   render() {
     return (
-      <ButtonBasics />
+      <Touchables />
     );
   }
 }
