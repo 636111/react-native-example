65884bc0fad721dddab58a876cf5874febcfa9b6
Carmen Domínguez Campillo <>
2018-05-22 22:23:30 +0200

    Add IScrolledDownAndWhatHappenedNextShockedMe component

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Touchables from './components/Touchables';
+import IScrolledDownAndWhatHappenedNextShockedMe from './components/IScrolledDownAndWhatHappenedNextShockedMe';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Touchables />
+      <IScrolledDownAndWhatHappenedNextShockedMe />
     );
   }
 }
